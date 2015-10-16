5d4e094b7b8b4583b5698d1575b9ae6d0dde39cd
dxeware <dxeware@gmail.com>
2015-10-16 07:12:45 -0700

    first attempt at gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+/*var connect = require('gulp-connect');
+var usemin = require('gulp-usemin');
+var minifyCss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var rev = require('gulp-rev');
+//var gutil = require('gulp-util');
+*/
+// connect
+/*gulp.task('connect', function() {
+  connect.server({
+    root: 'app/'
+  });
+}); */
+gulp.task('default', function() {
+  console.log("Running gulp default");
+});
